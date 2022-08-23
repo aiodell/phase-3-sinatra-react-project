3d32c9a2e2af78fa97f17bdaf24446e6d19ae4ec
Andrianna <andrianna.odell@gmail.com>
2022-08-23 10:05:26 -0400

    added get all Sponsors

--- app/controllers/sponsors_controller.rb
+++ app/controllers/sponsors_controller.rb
@@ -1,3 +1,9 @@
-class SponsorsController < Sinatra::Base
+class SponsorsController < ApplicationController
     set :default_content_type, 'application/json'
+
+    # get all sponsors
+    get "/sponsors" do
+        sponsors = Sponsor.all
+        sponsors.to_json
+    end
 end
