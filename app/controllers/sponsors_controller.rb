accc9a8e4fdccf35ed96f1468b24ab71c97ac846
Josephine Luu <josephinegluu@gmail.com>
2022-08-23 11:50:13 -0500

    CRUD for sponsors

--- app/controllers/sponsors_controller.rb
+++ app/controllers/sponsors_controller.rb
@@ -12,4 +12,34 @@ class SponsorsController < ApplicationController
         sponsor = Sponsor.find(params[:id])
         sponsor.to_json
     end
+
+    #delete a sponsor
+      delete '/sponsors/:id' do
+        sponsorss = Sponsor.find(params[:id])
+        sponsorss.destroy
+        sponsorss.to_json
+    end
+
+    #create a fox 
+    post '/sponsors' do 
+        sponsorsss = Sponsor.create(
+            name: params[:name],
+            image_url: params[:image_url],
+            location: params[:location],
+            num_of_foxes: params[:num_of_foxes]
+        )
+    end
+
+    #update a fox
+    patch '/sponsors/:id' do
+        sponsorssss = Sponsor.find(params[:id])
+        sponsorssss = Sponsor.update(
+            name: params[:name],
+            image_url: params[:image_url],
+            location: params[:location],
+            num_of_foxes: params[:num_of_foxes]
+        )
+        sponsorssss.to_json
+    end
+
 end
