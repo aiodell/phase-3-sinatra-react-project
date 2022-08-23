6e85bb3f5249a81e7a8fe7a1b422401bb0a02536
Josephine Luu <josephinegluu@gmail.com>
2022-08-23 11:04:15 -0500

    CRUD for fox

--- app/controllers/foxes_controller.rb
+++ app/controllers/foxes_controller.rb
@@ -7,17 +7,45 @@ class FoxesController < ApplicationController
         foxes.to_json
     end
 
-     #get all foxes
+    #get all foxes by ID
      get '/foxes/:id' do
         foxy = Fox.find(params[:id])
         foxy.to_json
     end
 
-    delete a fox
+    #delete a fox
     delete '/foxes/:id' do
-        foxy = Fox.find(params[:id])
-        foxy.destroy
-        foxy.to_json
+        foxie = Fox.find(params[:id])
+        foxie.destroy
+        foxie.to_json
+    end
+
+    #create a fox 
+    post '/foxes' do 
+        foxed = Fox.create(
+            name: params[:name],
+            age: params[:age],
+            favorite_toy: params[:favorite_toy],
+            personality: params[:personality],
+            image_url: params[:image_url],
+            sponsored: params[:sponsored],
+            sponsor_id: params[:sponsor_id]
+        )
+    end
+
+    #update a fox
+    patch '/foxes/:id' do
+        foxeys = Fox.find(params[:id])
+        foxeys.update(
+            name: params[:name],
+            age: params[:age],
+            favorite_toy: params[:favorite_toy],
+            personality: params[:personality],
+            image_url: params[:image_url],
+            sponsored: params[:sponsored],
+            sponsor_id: params[:sponsor_id]
+        )
+        foxeys.to_json
     end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -44,4 +44,3 @@ end
 
 puts "✅ Done seeding!"
 
-
