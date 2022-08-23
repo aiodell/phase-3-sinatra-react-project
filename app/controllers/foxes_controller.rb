823633578d8c549a0cdc138eed2f074fae0deb62
Josephine Luu <josephinegluu@gmail.com>
2022-08-23 09:23:37 -0500

    getting fox by id

--- app/controllers/foxes_controller.rb
+++ app/controllers/foxes_controller.rb
@@ -1,11 +1,23 @@
 class FoxesController < ApplicationController
     set :default_content_type, 'application/json'
 
+    #get all foxes
     get '/foxes' do
         foxes = Fox.all
         foxes.to_json
     end
 
+     #get all foxes
+     get '/foxes/:id' do
+        foxy = Fox.find(params[:id])
+        foxy.to_json
+    end
 
+    #delete a fox
+    # delete '/foxes/:id' do
+    #     foxy = Fox.find(params[:id])
+    #     foxy.destory
+    #     foxy.to_json
+    # end
 
 end
